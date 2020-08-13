0526421621955919876f6ac02ddb2cded70eb01b
Joseph <jtawil2015@gmail.com>
2020-08-12 18:00:13 -0800

    created get note route and create new note route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
+
+router.get("/api/notes", (req,res)=>
+{
+    fs.readFile("./db/db.json", "utf8", (err, data)=>{
+        if(err) throw err;
+        console.log(data);
+        res.send(data);
+    })
+});
+
+router.post("/api/notes", (req,res)=>{
+    const note = JSON.stringify(req.body);
+    console.log(note);
+    fs.appendFile("./db/db.json", note, (err, data)=>{
+        if(err) throw err;
+        res.send(data);
+    })
+});
 module.exports = router;
